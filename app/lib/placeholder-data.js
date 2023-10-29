2b3ca431aaae7d29460e1c3541fccd8240e37ed4
Luis Morales Bernal <luismcodes@gmail.com>
2023-10-28 18:48:28 -0600

    Define new styles Optimize fonts and imgs Create new route

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,5 +1,8 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
+
+// Each object represents a table in the DB.
+// Tables: users, customers, invoices, and revenue
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
