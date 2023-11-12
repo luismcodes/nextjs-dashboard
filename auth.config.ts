91d06a16b34f68a930ec1f74f5a46df9e2c87ff5
Luis Morales Bernal <luismcodes@gmail.com>
2023-11-12 15:51:55 -0600

    commit

--- auth.config.ts
+++ auth.config.ts
@@ -7,8 +7,11 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
+      const isLoggedIn = !!(auth?.user);
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      // console.log("auth: ", auth);
+      // console.log("request: ", nextUrl.pathname);
+      // console.log("isLoggedIn: ", !!(auth?.user));
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
